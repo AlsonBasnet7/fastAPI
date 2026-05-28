929d6f68096916f50432b7b04b3fc6a460ead206
AlsonBasnet07 <basnetalson121@gmail.com>
2026-05-28 07:07:22 +0545

    feat: used pydanti for data validation and fetched some test data in the system to see how the system communicates

---
+++ models.py
@@ -0,0 +1,15 @@
+
+# What is a Model?
+# A model is basically a blueprint for data.
+# It defines:
+# what data exists,
+# what type it is,
+# and how it should be stored in the database.
+from pydantic import BaseModel
+
+class Product(BaseModel):
+    id: int
+    name: str
+    description: str
+    price: float
+    quantity: int
